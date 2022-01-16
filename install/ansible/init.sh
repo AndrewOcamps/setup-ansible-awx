5610ae51d9a9cd7cedea08226e62259042f25959
Gerardo Ocampos <gerardoob97@hotmail.com>
2022-01-16 10:49:45 -0300

    fix init.sh for ansible

--- install/ansible/init.sh
+++ install/ansible/init.sh
@@ -7,20 +7,17 @@
 yum install -y epel-release
 yum install -y ansible
 yum install -y yum-utils device-mapper-persistent-data lvm2 ansible git python-devel python-pip python-docker-py vim-enhanced
+yum install -y python3
+yum install -y libselinux-python3
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce -y
 systemctl start docker
 systemctl enable docker
+pip3 install --upgrade pip
+pip3 install docker
+pip3 install docker-compose
 git clone -b 12.0.0 https://github.com/ansible/awx.git
 cd awx/
 git clone https://github.com/ansible/awx-logos.git
 cd installer/
-yum install -y python3
-cd /home/vagrant/awx/installer/
 ansible-playbook install.yml -i inventory
-yum install -y libselinux-python3
-pip3 install --upgrade pip
-pip3 install docker
-pip3 install docker-compose
-ansible-playbook install.yml -i inventory
-
